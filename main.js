a6cad81d0f9ff8362194eaf517575b47d1afd158
BotirovBahodir <botirovbahodir009@gmail.com>
2025-04-30 18:44:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+
+console.log(prompt("ismingizni kiriting:"));
+console.log(prompt("familiyangizni kiriting:"));
+console.log(prompt("yoshingizni kiriting:"));
+console.log(prompt("yaxshi korgan hayvoningizni kiriting:"));
+console.log(prompt("oyoq kiymingiz razmerini kiriting:"));
+console.log(prompt("yashash manzilingizni kiriting:"));
+console.log(prompt("yaxshi korgan moshinangizni kiriting:"));
+console.log(prompt("yaxshi korgan rangingizni kiriting:"));
+console.log(prompt("yaxshi korgan sport turini kiriting:"));
+console.log(prompt("yaxshi korgan kitobingizni kiriting:"));
+console.log(prompt("yaxshi korgan filimingizni kiriting:"));
+console.log(prompt("yaxshi korgan dasturlash tilini kiriting:"));
+
+
+
+
+
+
+alert(prompt("oyoq kiymingiz razmerini kiriting:"));
+alert(prompt("yashash manzilingizni kiriting:"));
+alert(prompt("yaxshi korgan moshinangizni kiriting:"));
+alert(prompt("yaxshi korgan rangingizni kiriting:"));
+alert(prompt("yaxshi korgan sport turini kiriting:"));
+alert(prompt("yaxshi korgan kitobingizni kiriting:"));
+alert(prompt("yaxshi korgan filimingizni kiriting:"));
+alert(prompt("yaxshi korgan dasturlash tilini kiriting:"));
+alert(prompt("yaxshi korgan veliklaringizni kiriting:"));
+alert(prompt("yaxshi korgan hayvoningizni kiriting:"));
+
+
+
+
+
+
+console.log(confirm("siz bilan tanishishimiz mumkinmi?"));
+console.log(confirm("yunsabodda yashaysanmi?"));
+console.log(confirm("sizning yoshingiz 18 dan katta?"));
+console.log(confirm("IT sohasida oqiysizmi?"));
+console.log(confirm("sizni ukangiz bormi?"));
+console.log(confirm("sizning ismingizni bilishimiz boladimi?"));
+console.log(confirm("yaxshi korgan hayvoningizni bilishimiz mumkinmi?"));
+console.log(confirm("yaxshi korgan sport turini bilishimiz mumkinmi?"));
+console.log(confirm("yaxshi korgan kitobingizni bilishimiz mumkinmi?"));
+console.log(confirm("yaxshi korgan filimingizni bilishimiz mumkinmi?"));
+console.log(confirm("yaxshi korgan dasturlash tilini bilishimiz mumkinmi?"));
+console.log(confirm("yaxshi korgan rangingizni bilishimiz mumkinmi?"));
+console.log(confirm("yaxshi korgan moshinangizni bilishimiz mumkinmi?"));
+console.log(confirm("yashash manzilingizni bilishimiz mumkinmi?"));
+console.log(confirm("yaxshi korgan veliklaringizni bilishimiz mumkinmi?"));
+console.log(confirm("yaxshi korgan kitobingizni bilishimiz mumkinmi?"));
+
+
+
+
+
+
+alert(confirm("yunsabodda yashaysanmi?"));
+alert(confirm("sizning yoshingiz 18 dan katta?"));
+alert(confirm("IT sohasida oqiysizmi?"));
+alert(confirm("sizni ukangiz bormi?"));
+alert(confirm("sizning ismingizni bilishimiz boladimi?"));
+alert(confirm("yaxshi korgan hayvoningizni bilishimiz mumkinmi?"));
+alert(confirm("yaxshi korgan sport turini bilishimiz mumkinmi?"));
+alert(confirm("yaxshi korgan kitobingizni bilishimiz mumkinmi?"));
+alert(confirm("yaxshi korgan filimingizni bilishimiz mumkinmi?"));
+alert(confirm("yaxshi korgan dasturlash tilini bilishimiz mumkinmi?"));
+
+
+
+
+
+
+
+
+
+
